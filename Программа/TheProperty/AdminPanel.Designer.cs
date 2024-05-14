d4ba3886fc5d78609336005c22ccce88eef18ad4
vaysten <anna.moreva.10@gmail.com>
2024-05-14 19:10:06 +0500

    added README.md

---
+++ Программа/TheProperty/AdminPanel.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace TheProperty
+{
+    partial class AdminPanel
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gunaLabel6 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel4 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaElipsePanel4 = new Guna.UI.WinForms.GunaElipsePanel();
+            this.DataTime = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel8 = new Guna.UI.WinForms.GunaLabel();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.gunaElipsePanel3 = new Guna.UI.WinForms.GunaElipsePanel();
+            this.CountUsers = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel3 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaElipsePanel2 = new Guna.UI.WinForms.GunaElipsePanel();
+            this.CountBuyAds = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel2 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaElipsePanel1 = new Guna.UI.WinForms.GunaElipsePanel();
+            this.CountAds = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
+            this.gunaLabel5 = new Guna.UI.WinForms.GunaLabel();
+            this.LayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.gunaElipsePanel4.SuspendLayout();
+            this.gunaElipsePanel3.SuspendLayout();
+            this.gunaElipsePanel2.SuspendLayout();
+            this.gunaElipsePanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gunaLabel6
+            // 
+            this.gunaLabel6.AutoSize = true;
+            this.gunaLabel6.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.gunaLabel6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.gunaLabel6.Location = new System.Drawing.Point(527, 97);
+            this.gunaLabel6.Name = "gunaLabel6";
+            this.gunaLabel6.Size = new System.Drawing.Size(97, 19);
+            this.gunaLabel6.TabIndex = 24;
+            this.gunaLabel6.Text = "Пользователи";
+            // 
+            // gunaLabel4
+            // 
+            this.gunaLabel4.AutoSize = true;
+            this.gunaLabel4.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.gunaLabel4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.gunaLabel4.Location = new System.Drawing.Point(8, 97);
+            this.gunaLabel4.Name = "gunaLabel4";
+            this.gunaLabel4.Size = new System.Drawing.Size(87, 19);
+            this.gunaLabel4.TabIndex = 23;
+            this.gunaLabel4.Text = "Объявления";
+            // 
+            // gunaElipsePanel4
+            // 
+            this.gunaElipsePanel4.BackColor = System.Drawing.Color.Transparent;
+            this.gunaElipsePanel4.BaseColor = System.Drawing.Color.White;
+            this.gunaElipsePanel4.Controls.Add(this.DataTime);
+            this.gunaElipsePanel4.Controls.Add(this.gunaLabel8);
+            this.gunaElipsePanel4.Location = new System.Drawing.Point(651, 10);
+            this.gunaElipsePanel4.Name = "gunaElipsePanel4";
+            this.gunaElipsePanel4.Radius = 12;
+            this.gunaElipsePanel4.Size = new System.Drawing.Size(165, 77);
+            this.gunaElipsePanel4.TabIndex = 22;
+            // 
+            // DataTime
+            // 
+            this.DataTime.BackColor = System.Drawing.Color.White;
+            this.DataTime.Font = new System.Drawing.Font("Segoe UI Semibold", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.DataTime.Location = new System.Drawing.Point(0, 29);
+            this.DataTime.Name = "DataTime";
+            this.DataTime.Size = new System.Drawing.Size(165, 31);
+            this.DataTime.TabIndex = 4;
+            this.DataTime.Text = "00.00.00";
+            this.DataTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaLabel8
+            // 
+            this.gunaLabel8.BackColor = System.Drawing.Color.White;
+            this.gunaLabel8.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel8.Location = new System.Drawing.Point(0, 12);
+            this.gunaLabel8.Name = "gunaLabel8";
+            this.gunaLabel8.Size = new System.Drawing.Size(165, 15);
+            this.gunaLabel8.TabIndex = 3;
+            this.gunaLabel8.Text = "Дата";
+            this.gunaLabel8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.AutoScroll = true;
+            this.flowLayoutPanel3.BackColor = System.Drawing.Color.White;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(531, 119);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(285, 405);
+            this.flowLayoutPanel3.TabIndex = 21;
+            // 
+            // gunaElipsePanel3
+            // 
+            this.gunaElipsePanel3.BackColor = System.Drawing.Color.Transparent;
+            this.gunaElipsePanel3.BaseColor = System.Drawing.Color.White;
+            this.gunaElipsePanel3.Controls.Add(this.CountUsers);
+            this.gunaElipsePanel3.Controls.Add(this.gunaLabel3);
+            this.gunaElipsePanel3.Location = new System.Drawing.Point(438, 10);
+            this.gunaElipsePanel3.Name = "gunaElipsePanel3";
+            this.gunaElipsePanel3.Radius = 12;
+            this.gunaElipsePanel3.Size = new System.Drawing.Size(207, 77);
+            this.gunaElipsePanel3.TabIndex = 19;
+            // 
+            // CountUsers
+            // 
+            this.CountUsers.BackColor = System.Drawing.Color.White;
+            this.CountUsers.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CountUsers.Location = new System.Drawing.Point(0, 23);
+            this.CountUsers.Name = "CountUsers";
+            this.CountUsers.Size = new System.Drawing.Size(207, 45);
+            this.CountUsers.TabIndex = 4;
+            this.CountUsers.Text = "000";
+            this.CountUsers.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaLabel3
+            // 
+            this.gunaLabel3.BackColor = System.Drawing.Color.White;
+            this.gunaLabel3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel3.Location = new System.Drawing.Point(0, 8);
+            this.gunaLabel3.Name = "gunaLabel3";
+            this.gunaLabel3.Size = new System.Drawing.Size(207, 15);
+            this.gunaLabel3.TabIndex = 3;
+            this.gunaLabel3.Text = "Количество пользователей";
+            this.gunaLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaElipsePanel2
+            // 
+            this.gunaElipsePanel2.BackColor = System.Drawing.Color.Transparent;
+            this.gunaElipsePanel2.BaseColor = System.Drawing.Color.White;
+            this.gunaElipsePanel2.Controls.Add(this.CountBuyAds);
+            this.gunaElipsePanel2.Controls.Add(this.gunaLabel2);
+            this.gunaElipsePanel2.Location = new System.Drawing.Point(225, 10);
+            this.gunaElipsePanel2.Name = "gunaElipsePanel2";
+            this.gunaElipsePanel2.Radius = 12;
+            this.gunaElipsePanel2.Size = new System.Drawing.Size(207, 77);
+            this.gunaElipsePanel2.TabIndex = 20;
+            // 
+            // CountBuyAds
+            // 
+            this.CountBuyAds.BackColor = System.Drawing.Color.White;
+            this.CountBuyAds.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CountBuyAds.Location = new System.Drawing.Point(0, 23);
+            this.CountBuyAds.Name = "CountBuyAds";
+            this.CountBuyAds.Size = new System.Drawing.Size(207, 45);
+            this.CountBuyAds.TabIndex = 3;
+            this.CountBuyAds.Text = "000";
+            this.CountBuyAds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaLabel2
+            // 
+            this.gunaLabel2.BackColor = System.Drawing.Color.White;
+            this.gunaLabel2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel2.Location = new System.Drawing.Point(0, 8);
+            this.gunaLabel2.Name = "gunaLabel2";
+            this.gunaLabel2.Size = new System.Drawing.Size(207, 15);
+            this.gunaLabel2.TabIndex = 2;
+            this.gunaLabel2.Text = "Объявление";
+            this.gunaLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaElipsePanel1
+            // 
+            this.gunaElipsePanel1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaElipsePanel1.BaseColor = System.Drawing.Color.White;
+            this.gunaElipsePanel1.Controls.Add(this.CountAds);
+            this.gunaElipsePanel1.Controls.Add(this.gunaLabel1);
+            this.gunaElipsePanel1.Location = new System.Drawing.Point(12, 10);
+            this.gunaElipsePanel1.Name = "gunaElipsePanel1";
+            this.gunaElipsePanel1.Radius = 12;
+            this.gunaElipsePanel1.Size = new System.Drawing.Size(207, 77);
+            this.gunaElipsePanel1.TabIndex = 18;
+            // 
+            // CountAds
+            // 
+            this.CountAds.BackColor = System.Drawing.Color.White;
+            this.CountAds.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CountAds.Location = new System.Drawing.Point(0, 23);
+            this.CountAds.Name = "CountAds";
+            this.CountAds.Size = new System.Drawing.Size(207, 45);
+            this.CountAds.TabIndex = 2;
+            this.CountAds.Text = "000";
+            this.CountAds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaLabel1
+            // 
+            this.gunaLabel1.BackColor = System.Drawing.Color.White;
+            this.gunaLabel1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaLabel1.Location = new System.Drawing.Point(0, 8);
+            this.gunaLabel1.Name = "gunaLabel1";
+            this.gunaLabel1.Size = new System.Drawing.Size(207, 15);
+            this.gunaLabel1.TabIndex = 1;
+            this.gunaLabel1.Text = "Количество объявлений";
+            this.gunaLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // gunaLabel5
+            // 
+            this.gunaLabel5.AutoSize = true;
+            this.gunaLabel5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gunaLabel5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gunaLabel5.ForeColor = System.Drawing.Color.Black;
+            this.gunaLabel5.Location = new System.Drawing.Point(96, 100);
+            this.gunaLabel5.Name = "gunaLabel5";
+            this.gunaLabel5.Size = new System.Drawing.Size(27, 15);
+            this.gunaLabel5.TabIndex = 25;
+            this.gunaLabel5.Text = "ALL";
+            this.gunaLabel5.Click += new System.EventHandler(this.gunaLabel5_Click);
+            // 
+            // LayoutPanel1
+            // 
+            this.LayoutPanel1.AutoScroll = true;
+            this.LayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.LayoutPanel1.ColumnCount = 3;
+            this.LayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.LayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.LayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.LayoutPanel1.Location = new System.Drawing.Point(12, 119);
+            this.LayoutPanel1.Name = "LayoutPanel1";
+            this.LayoutPanel1.RowCount = 4;
+            this.LayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.LayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.LayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.LayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.LayoutPanel1.Size = new System.Drawing.Size(499, 405);
+            this.LayoutPanel1.TabIndex = 26;
+            // 
+            // AdminPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.LayoutPanel1);
+            this.Controls.Add(this.gunaLabel5);
+            this.Controls.Add(this.gunaLabel6);
+            this.Controls.Add(this.gunaLabel4);
+            this.Controls.Add(this.gunaElipsePanel4);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.gunaElipsePanel3);
+            this.Controls.Add(this.gunaElipsePanel2);
+            this.Controls.Add(this.gunaElipsePanel1);
+            this.Name = "AdminPanel";
+            this.Size = new System.Drawing.Size(827, 533);
+            this.gunaElipsePanel4.ResumeLayout(false);
+            this.gunaElipsePanel3.ResumeLayout(false);
+            this.gunaElipsePanel2.ResumeLayout(false);
+            this.gunaElipsePanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaLabel gunaLabel6;
+        private Guna.UI.WinForms.GunaLabel gunaLabel4;
+        private Guna.UI.WinForms.GunaElipsePanel gunaElipsePanel4;
+        public Guna.UI.WinForms.GunaLabel DataTime;
+        private Guna.UI.WinForms.GunaLabel gunaLabel8;
+        public System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private Guna.UI.WinForms.GunaElipsePanel gunaElipsePanel3;
+        public Guna.UI.WinForms.GunaLabel CountUsers;
+        private Guna.UI.WinForms.GunaLabel gunaLabel3;
+        private Guna.UI.WinForms.GunaElipsePanel gunaElipsePanel2;
+        public Guna.UI.WinForms.GunaLabel CountBuyAds;
+        private Guna.UI.WinForms.GunaLabel gunaLabel2;
+        private Guna.UI.WinForms.GunaElipsePanel gunaElipsePanel1;
+        public Guna.UI.WinForms.GunaLabel CountAds;
+        private Guna.UI.WinForms.GunaLabel gunaLabel1;
+        private Guna.UI.WinForms.GunaLabel gunaLabel5;
+        public System.Windows.Forms.TableLayoutPanel LayoutPanel1;
+    }
+}
